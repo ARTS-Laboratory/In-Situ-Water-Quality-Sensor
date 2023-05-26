6d3eac3c6560868b9ea31be99779a5cc2b56f758
Mo994 <95874885+Mo994@users.noreply.github.com>
2023-05-25 21:18:20 -0400

    Updated PCB design and Arduino cpde

--- system_design/README.md
+++ system_design/README.md
@@ -8,11 +8,11 @@ An in situ water quality sensor for monitoring water quality
 1. This is a general dump of all the initial files. 
 
 ## V0.2
-1. This is just a clean up version of 0.1. 
+1. This is just a clean-up version of 0.1. 
 1. The pads on the 1026 resistors are too small
-1. move SMA connection over.
-1. Need to add ground plane to V0.3.
-1. Need to add SMA footprint on V0.3.
+1. move the SMA connection over.
+1. Need to add a ground plane to V0.3.
+1. Need to add the SMA footprint on V0.3.
 
 ## V0.3
 1. New designs for PCB boards. 
@@ -20,27 +20,28 @@ An in situ water quality sensor for monitoring water quality
 
 
 ## V 0.4.0
-1. Includes part designs necessary to assmeble water sensor package.
+1. Includes part designs necessary to assemble the water sensor package.
 2. Missing probe protector.
-3. Missing apple air tag mount.
+3. Missing Apple air tag mount.
 4. Need to adjust hold positioning for Solar Flap Fixture.
 
 
 ## V 0.4.5 
 1. Includes 2 component holders. One for TDS and pH, and another for TDS, pH, and Turbidity.
-2. Includes a probe protector that prevent damage to the ends of the probes.
+2. Includes a probe protector that prevents damage to the ends of the probes.
 3. Updated solar flaps to house solar flaps wired in parallel
-4. Solar Flap Fixture Cap updated to include a spot for apple air tag
+4. Solar Flap Fixture Cap updated to include a spot for Apple air tag
 5. Updated Solar Flap Fixture. (position for holes moved, height extended, and inside tabs reduced from 4 to 2)
-6. Need to make these parts out of a solid plastic pieces and leak test the package.
-7. PCB Design includes RTC Module, Micro SD Card Module.
-8. The PCB includes Footprints for the PH, TDS and Turbidity Sensors.
-9. Updated Arduino code to Measures the PH, TDS and Turbidity of the water.
-10. The code controls the sampling time and save the collected data on a micro SD card.
+6. Need to make these parts out of solid plastic pieces and leak-test the package.
+7. PCB Design includes RTC Module and Micro SD Card Module.
+8. The PCB includes Footprints for the PH, TDS, and Turbidity Sensors.
+9. Updated Arduino code to Measure the PH, TDS, and Turbidity of the water.
+10. The code controls the sampling time and saves the collected data on a micro SD card.
 
 ## V 0.4.6 
-1. These are general updates and a major reorganization all the files in V0.4.5.
-1. Includes 2 component holders. One for TDS and pH, and another for TDS, pH, and Turbidity.
+1. These are general updates and a major reorganization of all the files in V0.4.5.
+2. The updated PCB design includes footprints pH, Turbidity, TDS, and Temperature sensors.
+3. The Updated Arduino code.
 
 
 
--- system_design/V0.4.6/Code/In-Situ-Water-Quality-Sensor/In-Situ-Water-Quality-Sensor.ino
+++ system_design/V0.4.6/Code/In-Situ-Water-Quality-Sensor/In-Situ-Water-Quality-Sensor.ino
@@ -3,6 +3,8 @@
 #include <Wire.h>
 #include <EEPROM.h>
 #include <GravityTDS.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
 #define TdsSensorPin A1
 #include <DS323x.h>
 #ifdef USE_PULSE_OUT
@@ -12,8 +14,11 @@
   #include "ph_grav.h"             
   Gravity_pH pH = Gravity_pH(A0);   
 #endif
-                
-
+const int led = 5;
+const int SENSOR_PIN = 7;
+OneWire oneWire(SENSOR_PIN);         
+DallasTemperature tempSensor(&oneWire);             
+float tempCelsius;
 uint8_t user_bytes_received = 0;                
 const uint8_t bufferlen = 32;                   
 char user_data[bufferlen];                     
@@ -31,13 +36,14 @@ void save_temperature() {}
 
 void setup ()
 {
-  
+  pinMode(led,OUTPUT);
 
   Serial.begin(115200);
   gravityTds.setPin(TdsSensorPin);
   gravityTds.setAref(5.0);  //reference voltage on ADC, default 5.0V on Arduino UNO
   gravityTds.setAdcRange(1024);  //1024 for 10bit ADC;4096 for 12bit ADC
   gravityTds.begin();  //initialization
+  tempSensor.begin();
    Wire.begin();
     delay(1000);
   if (pH.begin()) {                                     
@@ -60,9 +66,15 @@ void setup ()
 
 void loop ()
 {
+  delay(3000);
+   
   gravityTds.setTemperature(temperature);  // set the temperature and execute temperature compensation
   gravityTds.update();  //sample and calculate
   tdsValue = gravityTds.getTdsValue();  // then get the value
+  
+   tempSensor.requestTemperatures();             // send the command to get temperatures
+  tempCelsius = tempSensor.getTempCByIndex(0);  // read temperature in Celsius
+  
   if (Serial.available() > 0) {                                                      
     user_bytes_received = Serial.readBytesUntil(13, user_data, sizeof(user_data));   
   }
@@ -72,31 +84,47 @@ void loop ()
     user_bytes_received = 0;                                                        
     memset(user_data, 0, sizeof(user_data));                                         
   }
+  int x = analogRead(A7);
+  x = constrain (x,0,750);
+  float NTU = map(x,0,750,100,0);
+  float y = pH.read_ph();
+  
+  DateTime now = rtc.now();
   Serial.print(tdsValue, 0);
-  Serial.print("ppm");
-  Serial.print(",");
-  Serial.print(" ");
-  Serial.print(pH.read_ph());
-  Serial.print("ph.m");
-  Serial.print(",");
-  Serial.print(" ");
-   DateTime now = rtc.now();
+    Serial.print(",");
+    Serial.print(" ");
+    Serial.print(y);
+    Serial.print(",");
+    Serial.print(" ");
+    Serial.print(NTU);
+    Serial.print(",");
+    Serial.print(" ");
+    Serial.print(tempCelsius);
+    Serial.print(",");
+    Serial.print(" ");
     Serial.println(now.timestamp());
-  //delay(300000);                                                      
-  delay(1000);
-
-  File dataFile = SD.open("package.csv", FILE_WRITE);
+  File dataFile = SD.open("two.csv", FILE_WRITE);
   if (dataFile) {
+    digitalWrite(led, HIGH);  
     dataFile.print(tdsValue, 0);
     dataFile.print(",");
     dataFile.print(" ");
-    dataFile.print(pH.read_ph());
+    dataFile.print(y);
+    dataFile.print(",");
+    dataFile.print(" ");
+    dataFile.print(NTU);
+    dataFile.print(",");
+    dataFile.print(" ");
+    dataFile.print(tempCelsius);
     dataFile.print(",");
     dataFile.print(" ");
     dataFile.println(now.timestamp());
+    delay(1000);
+    digitalWrite(led, LOW);
     dataFile.close();
   }
   else {
     Serial.println("error opening datalog.txt");
   }
+  //delay(3000);
 }
